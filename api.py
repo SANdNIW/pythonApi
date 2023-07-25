98b60818f23182f4477c798da89ff68869d5fbd9
SANdNIW <aip2539@gmail.com>
2023-07-25 14:13:56 +0700

    add route path

--- api.py
+++ api.py
@@ -1,11 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/api', methods=['GET'])
 def api():
     data = {'message': 'Hello, API!'}
     return jsonify(data)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return render_template('hello.html')
+
+
+@app.route('/page2', methods=['GET'])
+def page2():
+    return render_template('page2.html')
+
 
 if __name__ == '__main__':
     app.run()
