5b8c84fc1172aad830fef3d456b79d9dd8280964
SANdNIW <aip2539@gmail.com>
2023-07-26 15:35:48 +0700

    add vuejs

---
+++ frontVue/my-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mytime">
+    <MyTimepicker />
+  </div>
+</template>
+
+<script>
+import MyTimepicker from './components/MyTimepicker.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MyTimepicker
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontVue/my-app/src/components/MyTimepicker.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <vue-timepicker v-model="selectedTime" format="HH:mm" ref="timepicker" />
+        <button @click="confirmTime">Confirm Time</button>
+    </div>
+</template>
+  
+<script>
+
+import VueTimepicker from 'vue2-timepicker';
+
+export default {
+    components: {
+        VueTimepicker,
+    },
+    data() {
+        return {
+            selectedTime: '', // Holds the selected time in "HH:mm" format
+        };
+    },
+    methods: {
+        confirmTime() {
+            console.log('Selected Time:', this.selectedTime);
+        },
+    },
+};
+</script>
+
+  
+  
+  
+  
